cbc64a498afab7697c963049815a9a7dcf720e0d
digitalapplebee <digitalapplebee@gmail.com>
2019-08-26 04:09:54 +0300

    Add build

---
+++ build/precache-manifest.4ba06b4b001ba8043412fd94d6ddf194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5639762c42fca0284ff9da4db9404eec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "156f58ba126a6ddae803",
+    "url": "/static/css/2.03ce78f1.chunk.css"
+  },
+  {
+    "revision": "7a6bd848cb4b0b93a073",
+    "url": "/static/css/main.058aa9a7.chunk.css"
+  },
+  {
+    "revision": "156f58ba126a6ddae803",
+    "url": "/static/js/2.2a601a0b.chunk.js"
+  },
+  {
+    "revision": "7a6bd848cb4b0b93a073",
+    "url": "/static/js/main.7c7c4409.chunk.js"
+  },
+  {
+    "revision": "ac7855081f65c066be47",
+    "url": "/static/js/runtime~main.dd69cf33.js"
+  }
+]);
