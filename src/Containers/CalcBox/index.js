b4f83d4ac4006c9e743411fb66131ada46cc178a
Dmitry Priborovich <digitalapplebee@gmail.com>
2021-03-07 23:08:25 +0300

    Add convert function

--- src/Containers/CalcBox/index.js
+++ src/Containers/CalcBox/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import "./index.css";
 
 function CalcBox({ square }) {
+
+  const convertMtoKM = () => {
+    return (square / 1000).toFixed(3);
+  };
+
   return (
     <div className="calculation-box">
-      <p>Square figures: {square} m</p>
+      <p>Square figures: {convertMtoKM()} km</p>
     </div>
   );
 }
